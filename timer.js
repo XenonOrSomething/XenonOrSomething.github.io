b566cc3324ec685176310b3927d507ac7b351018
Xenon <143745702+XenonOrSomething@users.noreply.github.com>
2023-12-03 15:58:23 +0000

    Update timer.js

--- timer.js
+++ timer.js
@@ -21,7 +21,7 @@ if(dateCompare(endDate, new Date().getTime())){
   var distance = countDownDate - now;
 
   
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24) + 1);
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
