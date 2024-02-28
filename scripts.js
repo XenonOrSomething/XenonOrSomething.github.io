bd052b59dc6cd3485a276f5c940dc5953f38c1a4
Xenon <143745702+XenonOrSomething@users.noreply.github.com>
2024-02-28 20:50:19 +0000

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -6,4 +6,54 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
     // false for not mobile device
     console.log("thanks stackoverflow");
   }
+var img = document.getElementById("cat");
+var isDog = false;
+function loadcat(){
+  img.src = "loading.png"
+  if(isDog){
+    $.ajax({
+      url: "https://api.thedogapi.com/v1/images/search",
+      dataType: "json",
+      success: function(data){
+        var cat = data[0].url;
+        console.log(data.url)
+        console.log(cat)
+        img.src = cat;
+        console.log(data)
+      },
+      error: function(){
+        img.src = "error.png"
+        console.error("error loading goober :(")
+        console.log(data);
+      }
+      
+    })
+  }else{
+    $.ajax({
+      url: "https://api.thecatapi.com/v1/images/search",
+      dataType: "json",
+      success: function(data){
+        var cat = data[0].url;
+        console.log(data.url)
+        console.log(cat)
+        img.src = cat;
+        console.log(data)
+      },
+      error: function(){
+        img.src = "error.png"
+        console.error("error loading goober :(")
+        console.log(data);
+      }
+      
+    })
+  }
   
+}
+function dogcat(){
+  isDog = !isDog;
+  if(isDog){
+    document.getElementById("dogcat").innerHTML = "switch to cat"
+  }else{
+    document.getElementById("dogcat").innerHTML = "switch to dog"
+  }
+}
